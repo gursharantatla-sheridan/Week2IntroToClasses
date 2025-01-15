8490b614849f57cc8530b7449381c147c7a12d90
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-15 11:07:04 -0500

    main method

--- Week2IntroToClasses/Program.cs
+++ Week2IntroToClasses/Program.cs
@@ -6,6 +6,17 @@
         {
             Product p1 = new Product();
             Product p2 = new Product(101, "Lapto", 1200);
+
+
+            AccountWithMethods myAcc = new AccountWithMethods(500);
+
+            Console.WriteLine("Balance = " + myAcc.GetBalance().ToString("C"));
+
+            myAcc.Deposit(100);
+            Console.WriteLine("\nBalance = " + myAcc.GetBalance().ToString("C"));
+
+            myAcc.Withdraw(300);
+            Console.WriteLine("\nBalance = " + myAcc.GetBalance().ToString("C"));
         }
     }
 }
